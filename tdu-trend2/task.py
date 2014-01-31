545903c68d63ca0713f5901fc894ca4d1a4b72da
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2014-01-31 20:19:30 +0900

    test

--- tdu-trend2/task.py
+++ tdu-trend2/task.py
@@ -1,3 +1,3 @@
-import urllib2
-req = urllib2.Request("http://www42.atpages.jp/elzzup0105/trend/trend.php")
-f = urllib2.urlopen(req)
+import urllib
+req = urllib.urlopen("http://www42.atpages.jp/elzzup0105/trend/trend.php").read()
+#f = urllib2.urlopen(req)
